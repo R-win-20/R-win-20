2e2276f602f8330a4d8f738505cc2337e3f8190a
Rassel <rasse@hotmali.com>
2021-09-04 14:43:04 +0000

    Adding folders into repo

---
+++ lab1_template/orm/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Define your first model from here:
+from django.db import models
+
+# Test model
+class Test(models.Model):
+    name = models.CharField(max_length=30)
+
+class User(models.Model):
+    # CharField for user's first name
+    first_name = models.CharField(null=False, max_length=30, default='john')
+    # CharField for user's last name
+    last_name = models.CharField(null=False, max_length=30, default='doe')
+    # CharField for user's date for birth
+    dob = models.DateField(null=True)
---
+++ lab1_template/settings.py
@@ -0,0 +1,17 @@
+# Postgre SQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'mukagadieume',
+        'PASSWORD': 'ODk2My1tdWthZ2Fk',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'orm',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
